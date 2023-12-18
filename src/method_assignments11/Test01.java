1bfd98a2507a2c194d088a8d6f54504ec955b4f7
ADMIN <ADMIN@DESKTOP-US0QBG5>
2023-12-18 20:48:35 +0530

    static and non-static method

---
+++ src/method_assignments11/Test01.java
@@ -0,0 +1,15 @@
+package method_assignments11;
+
+public class Test01 {
+	int a = 12;
+	static int b = 24;
+
+	void test20 () {
+	int	c = 10;
+	System.out.println("this is void method");
+	}
+	static void test21 () {
+		System.out.println("this is static method");
+
+
+}}
---
+++ src/method_assignments11/Test02.java
@@ -0,0 +1,10 @@
+package method_assignments11;
+
+public class Test02 {
+public static void main(String[] args) {
+	Test01.test21();
+	
+	Test01 tes1 = new Test01();
+	tes1.test20();
+}
+}
